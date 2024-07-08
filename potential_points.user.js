e17e9669256d5d98fc2604958a9719afcb9f24c6
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-08 16:37:32 +0200

    Update potential_points.user.js
    - removed DataBridge

--- potential_points.user.js
+++ potential_points.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
-// @name        potential-Points
-// @namespace   potential-Points
+// @name        potential Points
+// @namespace   potential Points
 // @match       https://moodle.bbbaden.ch/course/user.php*
 // @match       https://moodle.bbbaden.ch/grade/report/user/index.php*
-// @match       https://moodle.bbbaden.ch/userscript/extensions
-// @version     6.2.0
+// @version     6.3.0
 //
 // @downloadURL https://github.com/BBBaden-Moodle-userscripts/potential-Points/raw/main/potential_points.user.js
 // @updateURL   https://github.com/BBBaden-Moodle-userscripts/potential-Points/raw/main/potential_points.user.js
@@ -24,33 +23,8 @@
 // @grant       GM_info
 //
 // @require     https://github.com/BBBaden-Moodle-userscripts/potentialPointsExtractLib/raw/main/extract-v1.lib.user.js
-// @require     https://github.com/black-backdoor/DataBridge/raw/main/DataBridge.lib.user.js
 // ==/UserScript==
 
-
-if(window.location.href == "https://moodle.bbbaden.ch/userscript/extensions"){
-    //------------------------ DataBridge ------------------------
-    // Create a new DataBridge
-    const UserScriptManagerCon = new Connection("BBBUserScriptManager");
-
-    // Register an event listener for the extensionInstalled event
-    Protocol.registerMessageType(UserScriptManagerCon, 'getInstalled', function (msg) {
-        UserScriptManagerCon.send({
-            "header": {
-                "receiver": msg.header.sender,
-                "protocolVersion": "1.0",
-                "messageType": "extensionInstalled",
-            },
-            "body": {
-                "script": {
-                    "scriptName": GM_info.script.name,
-                    "scriptVersion": GM_info.script.version,
-                }
-            }
-        });
-    });
-    return;
-}
 
 // ----------------- CONFIG -----------------
 // color if cell is empty
